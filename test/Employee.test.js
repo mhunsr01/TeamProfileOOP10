d5b1454fe2bc8401034e8b0ebf418b73c23cb5f3
Matt Sr Hunter <mhunsr@gmail.com>
2023-02-11 12:19:21 -0500

    completed files and ran app  seems to function properly and passes test

---
+++ test/Employee.test.js
@@ -0,0 +1,38 @@
+const Employee = require('../lib/Employee.js');
+
+// Class accepts (name, id, email)
+
+test('Checks for required Employee properties', () => {
+    const employee = new Employee('nobody', 1,'email@some.com');
+    expect(employee.name).toEqual(expect.any(String));
+    console.log(`Employee Name: ${employee.name}`)
+    expect(employee.id).toEqual(expect.any(Number));
+    console.log(`Employee ID: ${employee.id}`)
+    expect(employee.email).toEqual(expect.any(String));
+    console.log(`Employee Email: ${employee.email}`)
+});
+
+test('Gets employees name', () => {
+    const employee = new Employee('nobody', 1,'email@some.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+    console.log(`Employee getName(): ${employee.getName()}`)
+});
+
+test('Gets employees id', () => {
+    const employee = new Employee('nobody', 1,'email@some.com');
+    expect(employee.getId()).toEqual(expect.any(String));
+    console.log(`Employee getId(): ${employee.getId()}`)
+}); // getId returns a string btw
+
+test('Gets employees email', () => {
+    const employee = new Employee('nobody', 1,'email@some.com');
+    expect(employee.getEmail()).toEqual(expect.any(String));
+    console.log(`Employee getEmail(): ${employee.getEmail()}`)
+});
+
+test('Gets employees role', () => {
+    const employee = new Employee('nobody', 1,'email@some.com');
+    expect(employee.getRole()).toBe('Employee');
+    // Returns 'Employee'
+    console.log(`Employee getRole(): ${employee.getRole()}`)
+});
---
+++ test/Engineer.test.js
@@ -0,0 +1,33 @@
+const Engineer = require('../lib/Engineer.js');
+
+// Class accepts destructured object (name, id, email, github)
+const testObject = {name: 'Jeff', id: 2, email: "jeff@some.com", github: "its-jefe"}
+
+test('Checks for inherited Employee properties', () => {
+    const engineer = new Engineer(testObject);
+    expect(engineer.name).toEqual(expect.any(String));
+    console.log(`Engineer name: ${engineer.name}`)
+    expect(engineer.id).toEqual(expect.any(Number));
+    console.log(`Engineer id: ${engineer.id}`)
+    expect(engineer.email).toEqual(expect.any(String));
+    console.log(`Engineer email: ${engineer.email}`)
+});
+
+test('Checks for github', () => {
+    const engineer = new Engineer(testObject);
+    expect(engineer.github).toEqual(expect.any(String));
+    console.log(`Engineer github: ${engineer.github}`)
+});
+
+test('Checks for office number', () => {
+    const engineer = new Engineer(testObject);
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+    console.log(`Engineer getGithub(): ${engineer.getGithub()}`)
+});
+
+test('Gets Engineer role', () => {
+    const engineer = new Engineer(testObject);
+    expect(engineer.getRole()).toBe('Engineer');
+    console.log(`Engineer getRole(): ${engineer.getRole()}`)
+    // Returns 'Engineer'
+});
